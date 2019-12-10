457a282216f135b106b1f688145c2eb2646e7e45
MikkoPaajanen <paajasenmikko@gmail.com>
2019-12-10 11:36:40 +0200

    created app.js and moved app logic there

--- index.js
+++ index.js
@@ -1,39 +1,10 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
 const config = require('./utils/config')
-const Blog = require('./models/blog')
+
+const server = http.createServer(app)
 
 
-
-
-mongoose.connect(config.mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-
-app.use(cors())
-app.use(bodyParser.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
