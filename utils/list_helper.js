7d75a5b69d1b0f0589a596a2eb4ddc5269b9186f
MikkoPaajanen <paajasenmikko@gmail.com>
2019-12-10 14:00:12 +0200

    added tests and stuff

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,13 @@
 const dummy = (blogs) => {
+  return 1
+}
 
+const totalLikes = (blogs) => {
+  const reduced = blogs.reduce((sum, blog) => sum + blog.likes, 0)
+  return reduced
 }
 
 module.exports = {
-    dummy
+  dummy,
+  totalLikes
 }
