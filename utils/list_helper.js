f6fd851499ce8dc24aebcbf19d0472b4b0677dd9
MikkoPaajanen <paajasenmikko@gmail.com>
2019-12-10 22:13:19 +0200

    created a function that finds favorite blog

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,15 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0)
 
+
+const favoriteBlog = (blogs) => {
+  const mostLikes = () => blogs.reduce((prev, current) => (prev > current.likes) ? prev : current.likes, 0)
+  console.log(blogs.find(blog => blog.likes === mostLikes()))
+  return blogs.find(blog => blog.likes === mostLikes())
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
