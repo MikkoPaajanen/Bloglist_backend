6f1ffbd3330e3257aa5e7683c97a8e83d2ec23e7
MikkoPaajanen <paajasenmikko@gmail.com>
2019-12-10 12:16:17 +0200

    created blogs.js and moved routes there

--- app.js
+++ app.js
@@ -1,32 +1,16 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const blogRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
-const config = require('./utils/config')
-const Blog = require('./models/blog')
 
 mongoose.connect(config.mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogRouter)
 
 module.exports = app
