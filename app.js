19f9b9bf1fd66f4452f450787200e05e6a2007b0
MikkoPaajanen <paajasenmikko@gmail.com>
2019-12-18 14:07:19 +0200

    part 4 almost ready, only missing 1 of the earlier task

--- app.js
+++ app.js
@@ -14,10 +14,11 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 
 app.use(cors())
 app.use(bodyParser.json())
-
+app.use(middelware.tokenExtractor)
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
 app.use(middelware.unknownEndpoint)
 app.use(middelware.errorHandler)
 
