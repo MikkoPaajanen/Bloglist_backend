c5648a7798152b4da37bd52e30ff4085e0286b3c
MikkoPaajanen <paajasenmikko@gmail.com>
2019-12-18 10:44:26 +0200

    4.17 done

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
 const blogRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const mongoose = require('mongoose')
 const middelware = require('./utils/middelware')
 
 // connecting to database MongoDB Atlas
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 app.use(cors())
 app.use(bodyParser.json())
 
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', usersRouter)
 app.use(middelware.unknownEndpoint)
 app.use(middelware.errorHandler)
 
